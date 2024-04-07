3b718f0c4b1717fe89dedb0a98a717e5a90651a0
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-04-07 14:51:58 +0300

    build: setup build environment

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, crane, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.lib.${system};
+        commonArgs = {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          strictDeps = true;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
+          pname = "naucit-deps";
+          buildInputs = with pkgs; [];
+          nativeBuildInputs = with pkgs; [];
+        });
+        naucit = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          propagatedBuildInputs = with pkgs; [
+            nasm
+          ];
+        });
+      in {
+        checks = {
+          crate = naucit;
+        };
+
+        packages.default = naucit;
+
+        apps.default = utils.lib.mkApp {
+          drv = naucit;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+        };
+      }
+  );
+}
