d397b518d682ffd585472d6f1a6c3ddebf4679ad
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-04-07 18:07:09 +0300

    test: add tests for parser and fix bracket matching

--- src/parser.rs
+++ src/parser.rs
@@ -33,7 +33,7 @@ pub fn ast<T: AsRef<str>>(source: T) -> Vec<Token> {
                 let mut jump = 0;
                 let mut la = 1;
                 let mut ra = 0;
-                for (j, c) in chars[0..i].iter().enumerate() {
+                for (j, c) in chars[i + 1..].iter().enumerate() {
                     if *c == '[' {
                         la += 1;
                     }
@@ -47,7 +47,7 @@ pub fn ast<T: AsRef<str>>(source: T) -> Vec<Token> {
                         break;
                     }
                 }
-                Token::Break(i + jump)
+                Token::Break(i + 1 + jump)
             }
             ']' => {
                 // TODO: refactor this monstrosity
@@ -73,4 +73,24 @@ pub fn ast<T: AsRef<str>>(source: T) -> Vec<Token> {
             _ => Token::Comment,
         })
         .collect()
+}
+
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    type T = Token;
+
+    #[rstest]
+    #[case("", vec![])]
+    #[case(">", vec![T::Right])]
+    #[case("[>]", vec![T::Break(2), T::Right, T::Loop(0)])]
+    #[case(">>>>", vec![T::Right, T::Right, T::Right, T::Right])]
+    #[case(">+>+", vec![T::Right, T::Add, T::Right, T::Add])]
+    #[case("[[.-]]", vec![T::Break(5), T::Break(4), T::Write, T::Sub, T::Loop(1), T::Loop(0)])]
+    #[case("><+-.,[]", vec![T::Right, T::Left, T::Add, T::Sub, T::Write, T::Read, T::Break(7), T::Loop(6)])]
+    #[case("++++[>+.<-]", vec![T::Add, T::Add, T::Add, T::Add, T::Break(10), T::Right, T::Add, T::Write, T::Left, T::Sub, T::Loop(4)])]
+    fn test_parser(#[case] source: &str, #[case] expected: Vec<T>) {
+        assert_eq!(ast(source), expected);
+    }
 }
